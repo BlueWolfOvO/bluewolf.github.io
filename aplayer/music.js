ee07b47e47cae8f204c3a296b1aeda760ab32b42
BlueWolfOvO <1263635137@qq.com>
2019-01-14 00:09:21 +0800

    Site updated: 2019-01-14 00:09:21

--- aplayer/music.js
+++ aplayer/music.js
@@ -1,13 +1,11 @@
 $(document).pjax('a', '#musiclist',{
       timeout: 5000,
 	    fragment: '#musiclist',
-      cache: false,
 });
 const ap = new APlayer({
     container: document.getElementById('aplayer'),
     fixed: true,
     autoplay: true,
-    mutex: false,
 	  lrcType: 3,
     volume: 0.2,
     audio: [
